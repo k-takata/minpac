k-takata/minpac

commit 14f0a7db651cdcb5c80ee6f6b5b42b0919ea2409

Support "?" for wildcards

autoload/minpac/impl.vim
@@ -309,6 +309,7 @@ function! s:match_plugin(dir, packname, plugnames) abort
   let l:plugname = '\%(' . join(a:plugnames, '\|') . '\)'
   let l:plugname = substitute(l:plugname, '\.', '\\.', 'g')
   let l:plugname = substitute(l:plugname, '\*', '.*', 'g')
+  let l:plugname = substitute(l:plugname, '\?', '.', 'g')
   if l:plugname =~ '/'
     let l:pat = '/pack/' . a:packname . '/' . l:plugname . '$'
   else

commit 47d10eb0b0d92096cbc8f73523a7ccdb1969cb03

Fix minpac#clean()
Fix a bug introduced by 14f0a7db651cdcb5c80ee6f6b5b42b0919ea2409.
'?' is a literal character in Vim's regexp.

autoload/minpac/impl.vim
@@ -311,7 +311,7 @@ function! s:match_plugin(dir, packname, plugnames) abort
   let l:plugname = '\%(' . join(a:plugnames, '\|') . '\)'
   let l:plugname = substitute(l:plugname, '\.', '\\.', 'g')
   let l:plugname = substitute(l:plugname, '\*', '.*', 'g')
-  let l:plugname = substitute(l:plugname, '\?', '.', 'g')
+  let l:plugname = substitute(l:plugname, '?', '.', 'g')
   if l:plugname =~ '/'
     let l:pat = '/pack/' . a:packname . '/' . l:plugname . '$'
   else