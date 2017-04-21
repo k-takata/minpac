7321a7e14d2cd73bf7aab1ab6a83c824c816ab4c
K.Takata <kentkt@csc.jp>
2017-04-22 00:18:06 +0900

    curl option

--- tools/appveyor-dl.bat
+++ tools/appveyor-dl.bat
@@ -19,7 +19,7 @@ if "%DL%"=="yes" (
 	py tools\dl-kaoriya-vim.py --arch win64 --filename downloads\vim.zip --force
 	if not ERRORLEVEL 1 (
 		copy /y release-info.txt downloads > nul
-		curl -X DELETE -H "Authorization: Bearer %API_TOKEN%" %APPVEYOR_API_URL%api/projects/%APPVEYOR_ACCOUNT_NAME%/%APPVEYOR_PROJECT_SLUG%/buildcache
+		curl -X DELETE -i -H "Authorization: Bearer %API_TOKEN%" %APPVEYOR_API_URL%api/projects/%APPVEYOR_ACCOUNT_NAME%/%APPVEYOR_PROJECT_SLUG%/buildcache
 		echo %APPVEYOR_API_URL%api/projects/%APPVEYOR_ACCOUNT_NAME%/%APPVEYOR_PROJECT_SLUG%/buildcache
 	)
 )
