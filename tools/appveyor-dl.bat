536e77a8c6ff5c8e09f20b3c3150810272fe6d03
K.Takata <kentkt@csc.jp>
2017-04-22 00:00:12 +0900

    test

--- tools/appveyor-dl.bat
+++ tools/appveyor-dl.bat
@@ -4,7 +4,7 @@ if not exist downloads\nul mkdir downloads
 set DL=no
 dir downloads
 py tools\dl-kaoriya-vim.py -c > release-info.txt
-echo "foobar" > downloads\release-info2.txt
+echo "foobar" > downloads\release-info.txt
 if exist downloads\release-info.txt (
 	echo "old info:"
 	type downloads\release-info.txt
