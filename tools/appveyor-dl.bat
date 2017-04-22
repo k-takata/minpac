21eb4c796c333c0921b544304f55a928a1a7bbf8
K.Takata <kentkt@csc.jp>
2017-04-22 11:49:54 +0900

    show status

--- tools/appveyor-dl.bat
+++ tools/appveyor-dl.bat
@@ -1,17 +1,17 @@
 @echo off
 
 set CACHED=yes
-set DL=no
+set DL=yes
 py tools\dl-kaoriya-vim.py -c > release-info.txt
 if exist downloads\release-info.txt (
 	\cygwin64\bin\diff downloads\release-info.txt release-info.txt > nul
-	if ERRORLEVEL 1 set DL=yes
+	if not ERRORLEVEL 1 set DL=no
 ) else (
 	mkdir downloads
 	set CACHED=no
-	set DL=yes
 )
 if "%DL%"=="yes" (
+	echo Download the latest Vim.
 	py tools\dl-kaoriya-vim.py --arch win64 --filename vim.zip --force
 	if not ERRORLEVEL 1 (
 		move /y vim.zip downloads > nul
@@ -21,5 +21,7 @@ if "%DL%"=="yes" (
 			curl -X DELETE -i -H "Authorization: Bearer %API_TOKEN%" https://ci.appveyor.com/api/projects/%APPVEYOR_ACCOUNT_NAME%/%APPVEYOR_PROJECT_SLUG%/buildcache > nul
 		)
 	)
+) else (
+	echo Use cached version of Vim.
 )
 7z x downloads\vim.zip > nul
