937efc02333bd94dc181c659e635f5e493d73188
K.Takata <kentkt@csc.jp>
2017-07-11 19:50:28 +0900

    cygwin

---
+++ tools/appveyor.bat
@@ -0,0 +1,54 @@
+@echo off
+
+cd %APPVEYOR_BUILD_FOLDER%
+
+if "%1"=="" (
+  exit 1
+)
+set target=%1
+
+for %%i in (win64 cygwin64) do if "%env%"=="%%i" goto %env%_%target%
+
+echo Unknown build target.
+exit 1
+
+:win64_build
+:: ----------------------------------------------------------------------
+:: 64-bit Win32
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+call tools\appveyor-dl.bat
+@echo on
+%VIMPROG% -u NONE -c "redir @a | ver | 0put a | wq" ver.txt
+type ver.txt
+
+@echo off
+goto :eof
+
+:win64_test
+@echo on
+cd test
+nmake -f Make_win.mak VIMPROG=%VIMPROG%
+
+@echo off
+goto :eof
+
+
+:cygwin64_build
+:: ----------------------------------------------------------------------
+:: 64-bit Cygwin
+@echo on
+c:\cygwin64\setup-x86_64.exe -qnNdO -P vim,make
+PATH c:\cygwin64\bin;%PATH%
+set CHERE_INVOKING=yes
+bash -lc "$VIMPROG --version"
+
+@echo off
+goto :eof
+
+:cygwin64_test
+@echo on
+cd test
+bash -lc "make VIMPROG=$VIMPROG"
+
+@echo off
+goto :eof
